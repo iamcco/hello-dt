51d39921e053b3e29d8a9dcc4d126858cbe1fcf9
iamcco <ooiss@qq.com>
2021-11-10 11:20:51 +0800

    feat(editor): do not contrast image

--- app/src/utils.ts
+++ app/src/utils.ts
@@ -4,7 +4,6 @@ export function imageFilter(canvas: HTMLCanvasElement) {
   const ctx = canvas.getContext("2d")!;
   const image = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-  filters.contrast(image, { amount: 1.2 });
   filters.grayscale(image, { amount: 1 });
   ctx.putImageData(image, 0, 0);
   return canvas;
